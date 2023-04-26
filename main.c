8b43bdb0590d53221633549746c2dfe3301d91d1
MaxIhme <maxrehberg@posteo.de>
2023-04-26 09:18:19 +0200

    litte shorter main

--- main.c
+++ main.c
@@ -38,8 +38,6 @@ void	key_event(int key, t_info *info)
  */
 int main(void)
 {
-
-
 	// -----------------  wichtig MAIN behalten
 	t_info *info;
 
@@ -61,8 +59,6 @@ int main(void)
 	init_textures(info);
 	// ----------------- ENDE  wichtig MAIN behalten
 	
-
-
 	double tile[2];
 	
 	// ------  create test map  -- recognize from map file
@@ -98,23 +94,9 @@ int main(void)
 	info->p->cam_vec[X] = 0;
 	info->p->cam_vec[Y] = -1;
 
-	// print_2d_arr(info->map_int, 7, 7);
-
-
-
-
-
-
-
-
-
 
 	info->ceiling = argb(0, 0, 2, 255);  // replace - recognize from map file
 	info->floor = argb(0, 192, 192, 192);
-
-
-
-
 
 	// -----------------  wichtig MAIN behalten
 	init_mlx_window_first_screen(info);
@@ -122,52 +104,6 @@ int main(void)
 	mlx_loop(info->mlx_ptr);
 
 	// ----------------- ENDE  wichtig MAIN behalten
-
-	// t_img img_no;
-	// t_img img_ea;
-	// t_img img_so;
-	// t_img img_we;
-
-	// info.img = &img;
-	// info.north = &img_no;
-	// info.south = &img_so;
-	// info.east = &img_ea;
-	// info.west = &img_we;
-
-	
-
-
-
-	
-	
-
-	// img_2.img = mlx_new_image(info.mlx_ptr, DISPLAY_WIDTH, DISPLAY_HEIGHT);
-	// img_2.addr = mlx_get_data_addr(img_2.img, &img_2.bits_per_pixel, &img_2.line_length, &img_2.endian);
-
-	// int width = 64;
-	// int height = 64;
-
-
-	
-	// img_2.addr = mlx_get_data_addr(test, &img_2.bits_per_pixel, &img_2.line_length, &img_2.endian);
-	// char *tex_addr;
-	// t_img *wall_tex = &img_2;
-	// tex_addr = wall_tex->addr + ((int)(1) % 64 * wall_tex->line_length + 1 % 64 * (wall_tex->bits_per_pixel / 8));
-	// int color = *(int *)tex_addr;
-	// int color = get_color_from_img(&img_no, 17, 1);
-	// printf("ceiling = %d\n", info.ceiling);
-	// printf("color = %d\n", color);
-	// printf("test = %p\n", v_no);
-	// printf("addr = %p\n", img_no.addr);
-	// mlx_put_image_to_window(info.mlx_ptr, info.mlx_win, v_no, 100, 100);
-	
-	// mlx_put_image_to_window(window -> mlx_ptr, window -> win_ptr, window -> player_ptr, (run_width * window -> tile_width), (run_height * window -> tile_height));
-	// fill_background(color_fl, color_ce, &img_2);
-	// draw_wallshadows(info.dist_arr, &img);
-	// mlx_put_image_to_window(info.mlx_ptr, info.mlx_win, test, 0, 0);
-	
-	
-	
 }
 
 void	init_game(t_info *info)
