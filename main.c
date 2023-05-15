27a7bf7d1b59ca5139e4b305cda6d3fb2d362f68
MaxIhme <maxrehberg@posteo.de>
2023-05-15 11:32:06 +0200

    kurze main Funktion

--- main.c
+++ main.c
@@ -118,6 +118,28 @@ int	main(int argc, char **argv)
 	t_info  *info;
 
 	check_argc_and_dist_from_wall(argc);
+	info = init_process_game(); // init info, p, images, dist info, dist arr mit saved malloc
+ 	info->map_path = ft_strdup(argv[1]);
+	valid_map_extension(info); // max: ok (6te Zeile) (habe cleanup mit rein gepackt)
+	info->row = count_nb_row(info->map_path); //max says: unsave malloc in here
+	info->map = save_map(info); // returns 0 when malloc fails -> not save
+	parsing_(info); // -> parsing in unterfunktion
+	info->map_int = map_converter(info); // if malloc fails? - Max: keine Ahnung - so viele unsichere mallocs
+	convert_player_pos_dir(info);
+	init_mlx_and_textures(info); // nach parsing damit die Pfade zu den Texturen bekannt sind
+	init_mlx_window_first_screen(info);
+	mlx_key_hook(info->mlx_win, key_event, info);
+	mlx_hook(info->mlx_win, 17, 1L << 2, ft_free_destroy, info);
+	mlx_loop(info->mlx_ptr);
+ 	return (0);
+}
+
+/*
+int	main(int argc, char **argv)
+{
+	t_info  *info;
+
+	check_argc_and_dist_from_wall(argc);
 	// if (argc != 2)
  	// {
  	// 	message(ERROR_3);
@@ -163,3 +185,4 @@ int	main(int argc, char **argv)
 	mlx_loop(info->mlx_ptr);
  	return (0);
 }
+*/
